8fd2e8ab52fae658a52a63716b7f20e61fc02f74
vishnuvardhanreddythornala <vishnuvardhanreddythornala@gmail.com>
2026-04-13 16:39:49 +0530

    Remove omnicharge-ui from root tracking

--- omnicharge-ui/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: '',  // Set via CI/CD
-  razorpayKeyId: '',
-  googleClientId: '1084384475158-8sc296epqd9fj6hkplr0725hgbs6sl8h.apps.googleusercontent.com',
-};
