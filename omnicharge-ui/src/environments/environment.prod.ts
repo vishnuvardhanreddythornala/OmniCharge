e85a026c2c21c72fc642d4344c35492cc673a794
vishnuvardhanreddythornala <vishnuvardhanreddythornala@gmail.com>
2026-04-23 14:25:01 +0530

    Updated Razorpay Credentials

--- omnicharge-ui/src/environments/environment.prod.ts
+++ omnicharge-ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: '',  // Set via CI/CD
-  razorpayKeyId: 'rzp_live_SUvKCS3WVGspwp',
+  razorpayKeyId: 'rzp_live_SgV8VMOB5gIRcd',
   googleClientId: '1084384475158-8sc296epqd9fj6hkplr0725hgbs6sl8h.apps.googleusercontent.com',
 };
