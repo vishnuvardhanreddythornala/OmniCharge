7f829e84688f4b81993701740dfacb5830677352
vishnuvardhanreddythornala <vishnuvardhanreddythornala@gmail.com>
2026-05-02 10:05:03 +0530

    chore: switch backend to Azure VM + add VM deployment scripts

--- omnicharge-ui/src/environments/environment.prod.ts
+++ omnicharge-ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://api-gateway.ambitiousisland-8a9ff725.centralindia.azurecontainerapps.io',
+  apiBaseUrl: 'https://omnicharge.centralindia.cloudapp.azure.com',
   razorpayKeyId: 'rzp_live_SgV8VMOB5gIRcd',
   googleClientId: '1084384475158-8sc296epqd9fj6hkplr0725hgbs6sl8h.apps.googleusercontent.com',
 };
