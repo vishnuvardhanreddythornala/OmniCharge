d36a90d6296ef608e0445ed0f99e3de91f9d8cb7
vishnuvardhanreddythornala <vishnuvardhanreddythornala@gmail.com>
2026-05-04 15:29:00 +0530

    docs: add business presentation slide deck and clean up deleted files

--- HashGen.java
+++
@@ -1,9 +0,0 @@
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-public class HashGen {
-    public static void main(String[] args) {
-       
-        System.out.println("Wait, I will use Java library.");
-    }
-}
